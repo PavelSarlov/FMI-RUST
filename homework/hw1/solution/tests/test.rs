46c5178360fb5ade0ee77ae1f8c5645f67b8c124
Pavel Sarlov <sarlovpavel@gmail.com>
2021-10-23 15:57:47 +0300

    update tests

--- homework/hw1/solution/tests/test.rs
+++ homework/hw1/solution/tests/test.rs
@@ -4,14 +4,17 @@ use solution::*;
 
 #[test]
 fn test_basic() {
-    let input: Vec<char> = "GC".chars().collect();
+    let input: Vec<char> = "TGAC".chars().collect();
     let counter = counts(&input);
 
     assert_eq!(counter.g, 1);
     assert_eq!(counter.c, 1);
-    assert_eq!(counter.a, 0);
-    assert_eq!(counter.t, 0);
+    assert_eq!(counter.a, 1);
+    assert_eq!(counter.t, 1);
 
-    assert_eq!(dna_complement(&input),         vec!['C', 'G']);
-    assert_eq!(reverse_rna_complement(&input), vec!['G', 'C']);
+//    assert_eq!(dna_complement(&input),         vec!['C', 'G']);
+//    assert_eq!(reverse_rna_complement(&input), vec!['G', 'C']);
+
+    assert_eq!(dna_complement(&input),          vec!['A', 'C', 'T', 'G']);
+    assert_eq!(reverse_rna_complement(&input),  vec!['G', 'U', 'C', 'A'])
 }
